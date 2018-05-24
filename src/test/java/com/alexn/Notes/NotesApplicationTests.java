d3bbf33436f5bcd1be7c6b377a28365243b07cc5
alexxxp <mail7115@mail.ru>
2018-05-24 20:11:11 +0700

    first

--- src/test/java/com/alexn/Notes/NotesApplicationTests.java
+++ src/test/java/com/alexn/Notes/NotesApplicationTests.java
@@ -1,16 +1,24 @@
 package com.alexn.Notes;
 
+import com.alexn.Notes.controllers.MainController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NotesApplicationTests {
 
+	@Autowired
+	private MainController mainController;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(mainController).isNotNull();
 	}
 
 }
